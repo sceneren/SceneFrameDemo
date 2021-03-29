feeec0786f068831af2b52e85a32622fb08afb4c
scene <bergeqhmk09@gmail.com>
2021-03-29 11:37:33 +0800

    xx

--- Lib-Network/src/main/java/wiki/scene/lib_network/livedata/FastLoadingObserver.kt
+++ Lib-Network/src/main/java/wiki/scene/lib_network/livedata/FastLoadingObserver.kt
@@ -5,9 +5,13 @@ import com.kongzue.dialogx.dialogs.WaitDialog
 import wiki.scene.lib_network.R
 
 abstract class FastLoadingObserver<T> : FastObserver<T>() {
+    private val loadingStr by lazy {
+        StringUtils.getString(R.string.lib_network_loading)
+    }
+
     override fun onStart() {
         super.onStart()
-        WaitDialog.show(StringUtils.getString(R.string.lib_network_loading))
+        WaitDialog.show(loadingStr)
     }
 
     override fun onFinish() {
