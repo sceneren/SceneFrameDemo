6d0c17a991c37235d74f62fafcebef4253ea0c47
scene <bergeqhmk09@gmail.com>
2021-06-07 11:37:36 +0800

    koin替换为hilt

--- app/src/main/java/wiki/scene/demo/adapter/ViewPager2Adapter.kt
+++ app/src/main/java/wiki/scene/demo/adapter/ViewPager2Adapter.kt
@@ -4,11 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import wiki.scene.lib_base.base_fg.fragmentvisibility.RxVisibilityFragment
-import javax.inject.Inject
 
 class ViewPager2Adapter constructor(
     mActivity: AppCompatActivity,
-    private val fragmentList: MutableList<RxVisibilityFragment>
+    private val fragmentList: MutableList<RxVisibilityFragment> = mutableListOf()
 ) : FragmentStateAdapter(mActivity) {
     override fun getItemCount(): Int {
         return fragmentList.size
